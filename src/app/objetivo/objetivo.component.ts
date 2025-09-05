104071d23acddaa391a4c27457e1dd8510460c4b
Olimac1954 <andresayala1954@hotmail.com>
2025-09-05 18:13:45 -0500

    5 commit con estilos de contenido sin responsive

--- src/app/objetivo/objetivo.component.ts
+++ src/app/objetivo/objetivo.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrl: './objetivo.component.css'
 })
 export class ObjetivoComponent {
+ objetivo = {
+    nombre: 'Conocer mi sobrino',
+    categoria: 'Bienestar',
+    fecha: 'Diciembre/2022',
+    monto: 6000000,
+    actual: 0
+  };
 
+  productos = [
+    {
+      nombre: 'FIC Efectivo',
+      id: '#60000986159',
+      ahorro: 2120000,
+      recomendacion: 'Te recomendamos aportar $388.000 mensuales para lograrlo en diciembre del 2022.'
+    },
+    {
+      nombre: 'FIC Efectivo',
+      id: '#802300098619',
+      ahorro: 7120000,
+      info: '¡Súper ya tienes los $6.000.000 para tu objetivo! Úsalos cuando lo desees.'
+    },
+    {
+      nombre: 'Seguro de vida CREA',
+      id: '#710000986159',
+      ahorro: 7120000,
+      info: '¡Ya tienes los $6.000.000 para tu objetivo! Úsalos desde el 16/nov/27 que finaliza la vigencia de tu póliza.'
+    }
+  ];
 }
